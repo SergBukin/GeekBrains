450cac8be78c2ce803a1d2309dafc3281fee9ce3
Sergey Bukin <sergey.bukin@gmail.com>
2022-10-20 22:53:58 +0300

    Commit 10 Seminar 5)

--- lessons/lesson2/Program.cs
+++ lessons/lesson2/Program.cs
@@ -1,130 +1,235 @@
-﻿// //  Знакомство с функциями и массивами
+﻿//  Знакомство с функциями и массивами
 // __________________________________________________
-// // Задача №1: Функция Мах
-// ____________________________________________________
-// int Max(int x, int y, int z)
+//  Задача №1: Функция Мах
+// // // ____________________________________________________
+// // // int Max(int x, int y, int z)
+// // // {
+// // //     int result = x;
+// // //     if(y> result) result = y;
+// // //      if(z> result) result = z;
+// // //     return result;
+// // // }
+
+// // // int a1 = 1;
+// // // int b1 = 2;
+// // // int c1 = 6;
+
+// // // int a2 = 200;
+// // // int b2 = 3;
+// // // int c2 = 7;
+
+// // // int a3 = 4;
+// // // int b3 = 5;
+// // // int c3 = 89;
+
+// // // // *int max1 = Max(a1, b1, c1);
+// // // // *int max2 = Max(a2, b2, c2);
+// // // // *int max3 = Max(a3, b3, c3);
+
+// // // int max = Max(
+// // //     Max(a1, b1, c1), 
+// // //     Max(a2, b2, c2),
+// // //     Max(a3, b3, c3));
+
+// // // Console.Write("max = ");
+// // // Console.WriteLine(max);
+
+// // // ***************************************
+// // // Задача №2: Массивы
+
+// // // ***ТИП ДАННЫХ[] ИМЯ = {Значение1, Значение2, ...}
+// // // ***int[] array = {9, 28, 1, 32, 1990}
+// // // ***int[] array = {0, 0, 0, 0, 0}
+// // // ***int[] array = new int[5]
+// // // ***int[] array = new int[5]{9, 28, 1, 32, 1990};
+// // // _____________________________________________________________
+
+// // // int Max(int arg1, int arg2, int arg3)
+// // // {
+// // //     int result = arg1;
+// // //     if(arg2 > result) result = arg2;
+// // //     if(arg3 > result) result = arg3;
+// // //     return result;
+// // // }
+
+// // // int[] array = {11, 21, 31, 41, 15, 61, 17, 18, 19};
+
+// // // int max = Max(Max(array[0], array[1], array[2]), Max(array[3], array[4], array[5]), Max(array[6], array[7], array[8]));
+
+// // // Console.WriteLine(max);
+
+// // // *************************************************************
+
+// // // int[] array = {11, 22, 31, 43, 50, 16, 72, 81};
+// // // int n = array.Length;
+// // // int index = 0;
+// // // int find = 31;
+
+// // // while (index < n)
+// // // {
+// // //   if (array[index] == find)
+// // //   System.Console.WriteLine(index);
+// // //   index++;
+// // // }
+
+// // // **********************************************************
+
+// // Console.Clear();
+// // void FillArray(int[] collection)
+
+// // {
+// //    int length = collection.Length;
+// //    int index = 0;
+// //    while (index < length)
+// //    {
+// //     collection[index] = new Random().Next(1, 10);
+// //     index++;
+// //    }
+// // }
+// // void PrintArray(int[] col)
+// // {
+// //     int count = col.Length;
+// //     int position = 0;
+
+// //     while (position < count)
+// //     {
+// //     System.Console.WriteLine(col[position]);
+// //     position++;
+// //     }
+// // }
+
+// // int IndexOf(int[] collection, int find)
+// // {
+
+// //     int count = collection.Length;
+// //     int index = 0;
+// //     int position = -1;
+
+// //     while (index < count)
+// //     {
+// //        if(collection[index] == find)
+// //        {
+// //           position = index;
+// //           break;
+// //        }
+// //         index++;
+// //     }
+// //     return position;
+// // }
+
+// // int[] array = new int[10];
+
+// // FillArray(array);
+// // PrintArray(array);
+
+// // System.Console.WriteLine();
+
+// // int pos = IndexOf(array, 4);
+// // System.Console.WriteLine(pos);
+
+// // // ****************************************************
+
+// int[] CreateArray(int size, int min, int max)
 // {
-//     int result = x;
-//     if(y> result) result = y;
-//      if(z> result) result = z;
-//     return result;
+// 	int[] array = new int[size];
+// 	Random rnd = new Random();
+
+// 	for (int i = 0; i < size; i++)
+// 	{
+// 		array[i] = rnd.Next(min, max);
+// 	}
+
+// 	return array;
 // }
 
-// int a1 = 1;
-// int b1 = 2;
-// int c1 = 6;
-
-// int a2 = 200;
-// int b2 = 3;
-// int c2 = 7;
-
-// int a3 = 4;
-// int b3 = 5;
-// int c3 = 89;
-
-// // *int max1 = Max(a1, b1, c1);
-// // *int max2 = Max(a2, b2, c2);
-// // *int max3 = Max(a3, b3, c3);
-
-// int max = Max(
-//     Max(a1, b1, c1), 
-//     Max(a2, b2, c2),
-//     Max(a3, b3, c3));
-
-// Console.Write("max = ");
-// Console.WriteLine(max);
-
-// ***************************************
-// Задача №2: Массивы
-
-// ***ТИП ДАННЫХ[] ИМЯ = {Значение1, Значение2, ...}
-// ***int[] array = {9, 28, 1, 32, 1990}
-// ***int[] array = {0, 0, 0, 0, 0}
-// ***int[] array = new int[5]
-// ***int[] array = new int[5]{9, 28, 1, 32, 1990};
-// _____________________________________________________________
-
-// int Max(int arg1, int arg2, int arg3)
+// int[] GetReverseArray(int[] array)
 // {
-//     int result = arg1;
-//     if(arg2 > result) result = arg2;
-//     if(arg3 > result) result = arg3;
-//     return result;
+// 	int[] reversedArray = new int[array.Length];
+
+// 	for (int i = 0; i < array.Length; i++)
+// 	{
+// 		reversedArray[i] = -array[i];
+// 	}
+
+// 	return reversedArray;
 // }
 
-// int[] array = {11, 21, 31, 41, 15, 61, 17, 18, 19};
-
-// int max = Max(Max(array[0], array[1], array[2]), Max(array[3], array[4], array[5]), Max(array[6], array[7], array[8]));
-
-// Console.WriteLine(max);
-
-// *************************************************************
-
-// int[] array = {11, 22, 31, 43, 50, 16, 72, 81};
-// int n = array.Length;
-// int index = 0;
-// int find = 31;
-
-// while (index < n)
+// string ArrayToString(int[] array)
 // {
-//   if (array[index] == find)
-//   System.Console.WriteLine(index);
-//   index++;
+// 	string result = "[";
+
+// 	foreach (int item in array)
+// 	{
+// 		result += $"{item}, ";
+// 	}
+
+// 	result.Trim(',');
+// 	result += "]";
+
+// 	return result;
 // }
 
-// **********************************************************
+// int[] array = CreateArray(10, -10, 10);
 
-Console.Clear();
-void FillArray(int[] collection)
+// Console.WriteLine(ArrayToString(array));
+// Console.WriteLine(ArrayToString(GetReverseArray(array)));
 
+//  **********************************************************************
+
+int[] CreateArray(int size, int min, int max)
 {
-   int length = collection.Length;
-   int index = 0;
-   while (index < length)
-   {
-    collection[index] = new Random().Next(1, 10);
-    index++;
-   }
-}
-void PrintArray(int[] col)
-{
-    int count = col.Length;
-    int position = 0;
+	int[] array = new int[size];
+	Random rnd = new Random();
 
-    while (position < count)
-    {
-    System.Console.WriteLine(col[position]);
-    position++;
-    }
+	for (int i = 0; i < size; i++)
+	{
+		array[i] = rnd.Next(min, max);
+	}
+
+	return array;
 }
 
-int IndexOf(int[] collection, int find)
+int[] GetReverseArray(int[] array)
 {
+	int[] reversedArray = new int[array.Length];
 
-    int count = collection.Length;
-    int index = 0;
-    int position = -1;
+	for (int i = 0; i < array.Length; i++)
+	{
+		reversedArray[i] = -array[i];
+	}
 
-    while (index < count)
-    {
-       if(collection[index] == find)
-       {
-          position = index;
-          break;
-       }
-        index++;
-    }
-    return position;
+	return reversedArray;
 }
 
-int[] array = new int[10];
+string ArrayToString(int[] array)
+{
+	string result = "[";
 
-FillArray(array);
-PrintArray(array);
+	foreach (int item in array)
+	{
+		result += $"{item}, ";
+	}
 
-System.Console.WriteLine();
+	result += "]";
 
-int pos = IndexOf(array, 4);
-System.Console.WriteLine(pos);
+	return result;
+}
 
-// ****************************************************
+bool FindNumber(int[] array, int number)
+{
+	for (int i = 0; i < array.Length; i++)
+	{
+		if (array[i] == number)
+			return true;
+	}
+
+	return false;
+}
+
+int[] array = CreateArray(20, -5, 5);
+
+int findNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine(ArrayToString(array));
+Console.WriteLine(ArrayToString(GetReverseArray(array)));
+Console.WriteLine(FindNumber(array, findNumber));
---
+++ seminars/seminar5/Program.cs
@@ -0,0 +1,265 @@
+﻿// Разбор домашнего задания
+// int[] CreateArray()
+// {
+//  int[] ints = new int[8];
+//  Random random = new Random();
+//  for (int i = 0; i < ints.Length; i++)
+//  {
+//     ints[i] = random.Next();
+//  }
+//  return ints;
+// }
+
+// string GetArrayString(int[] array)
+// {
+//     string Result = "";
+//     for 
+// }
+
+// *************************************************************
+// int[] CreateArray(int size, int minValue, int MaxValue)
+// {
+//     int[] array = new int[size];
+//     Random random = new Random();
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = random.Next(minValue, MaxValue);
+//     }
+//     return array;
+// }
+
+// int GetSumPositiveElementsFromArray(int[] array)
+// {
+//     int sum = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if(array[i] > 0) sum = sum + array[i];
+//     }
+//     return sum;
+// }
+
+// int GetSumNegativeElementsFromArray(int[] array)
+// {
+//     int sum = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if(array[i] < 0) sum = sum + array[i];
+//     }
+//     return sum;
+// }
+
+// string GetArray(int[] array)
+// {
+//     string sum = string.Empty;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         sum = sum + $"{ array[i]}, ";
+//     }
+//     return sum;
+// }
+
+// int[] array  = CreateArray(12, -9, 10);
+
+// System.Console.WriteLine(GetArray(array));
+// System.Console.WriteLine("Сумма положительных элементов равна = " + GetSumPositiveElementsFromArray(array));
+// System.Console.WriteLine("Сумма отрицательных элементов равна = " + GetSumNegativeElementsFromArray(array));
+
+// *******************************
+
+// int[] CreateArray(int size, int min, int max)
+// {
+// 	int[] array = new int[size];
+// 	Random rnd = new Random();
+
+// 	for (int i = 0; i < size; i++)
+// 	{
+// 		array[i] = rnd.Next(min, max);
+// 	}
+
+// 	return array;
+// }
+
+// int[] GetReverseArray(int[] array)
+// {
+// 	int[] reversedArray = new int[array.Length];
+
+// 	for (int i = 0; i < array.Length; i++)
+// 	{
+// 		reversedArray[i] = -array[i];
+// 	}
+
+// 	return reversedArray;
+// }
+
+// string ArrayToString(int[] array)
+// {
+// 	string result = "[";
+
+// 	foreach (int item in array)
+// 	{
+// 		result += $"{item}, ";
+// 	}
+
+// 	result += "]";
+
+// 	return result;
+// }
+
+// bool FindNumber(int[] array, int number)
+// {
+// 	for (int i = 0; i < array.Length; i++)
+// 	{
+// 		if (array[i] == number)
+// 			return true;
+// 	}
+
+// 	return false;
+// }
+
+// int[] array = CreateArray(20, -5, 5);
+
+// int findNumber = int.Parse(Console.ReadLine());
+
+
+// Console.WriteLine(ArrayToString(array));
+// Console.WriteLine(ArrayToString(GetReverseArray(array)));
+// Console.WriteLine(FindNumber(array, findNumber));
+
+// ************************************************************
+
+
+// Задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, 
+// значения которых лежат в отрезке [10,99]. 
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+// ________________________________________________________________________
+
+// int[] CreateArray(int size, int min, int max)
+// {
+// 	int[] array = new int[size];
+// 	Random rnd = new Random();
+    
+
+// 	for (int i = 0; i < size; i++)
+// 	{
+// 		array[i] = rnd.Next(min, max);
+// 	}
+
+// 	return array;
+// }
+
+// string ArrayToString(int[] array)
+// {
+// 	string result = "[";
+
+// 	foreach (int item in array)
+// 	{
+// 		result += $"{item}, ";
+// 	}
+
+// 	result += "]";
+
+// 	return result;
+// }
+
+
+// int GetNumbersOfElements(int[] array)
+// {
+// 	int[] reversedArray = new int[array.Length];
+//     int count = 0;
+// 	for (int i = 0; i < array.Length; i++)
+// 	{
+//         int result = array[i];
+        
+// 		if (result > 10 && result <100)
+//         {
+//             count++;
+//         }
+// 	}
+
+// 	return count;
+// }
+// int[] array = CreateArray(123, -5, 150);
+
+// Console.WriteLine(ArrayToString(array));
+// Console.WriteLine(GetNumbersOfElements(array));
+
+// ************************************************************
+
+// int[] CreateArray(int size, int min, int max) 
+// { int[] array = new int[size];
+//  Random rnd = new Random(); 
+//  for (int i = 0; i < size; i++) 
+//  { array[i] = rnd.Next(min, max); 
+//  }
+//     return array;
+// }
+// int Qelements(int[] array)
+// {int counter = 0;
+    
+//     foreach (int element in array)
+//     {
+//         if ( element > 9 & element < 99) counter ++;    }
+//     return counter;
+// }
+// string GetArray1(int[] array) 
+// { 
+//     string sum = string.Empty; 
+//     for (int i = 0; i < array.Length; i++) 
+//     { 
+//         sum = sum + $"{array[i]}, "; 
+//     }
+        
+//          return sum; 
+// }
+// int[] array = CreateArray(123, -150, 150);
+// System.Console.WriteLine(GetArray1(array));
+// System.Console.WriteLine(Qelements(array));
+
+// int[] ProzElem(int[] array)
+
+// {
+//     int[] arrayP =new int[array.Length/2 +1];
+//     int i;
+//     for (i =0; i < array.Length/2; i++)
+//     {
+//         arrayP[i] = array[i]*array[array.Length-i-1];
+//     }  
+//     if (array.Length % 2 != 0) arrayP[arrayP.Length-1]=array[i];
+
+//     return arrayP;
+// }
+// array = CreateArray(5, 0, 10);
+// System.Console.WriteLine(GetArray1(array));
+// System.Console.WriteLine(GetArray1(ProzElem(array)));
+
+// **********************************************************************
+
+// ...Добавка (№2 задание)
+
+// int FilterArray(int[] array, int min, int max)
+// {
+// 	int count = 0;
+
+// 	for (int i = 0; i < array.Length; i++)
+// 	{
+// 		if (array[i] > min && array[i] < max)
+// 		{
+// 			count++;
+// 		}
+// 	}
+
+// 	return count;
+// }
+
+// int[] array = CreateArray(1230, -10, 150);
+
+// //int findNumber = int.Parse(Console.ReadLine());
+
+// Console.WriteLine(ArrayToString(array));
+// //Console.WriteLine(ArrayToString(GetReverseArray(array)));
+// //Console.WriteLine(FindNumber(array, findNumber));
+// Console.WriteLine(FilterArray(array, 9, 100));
