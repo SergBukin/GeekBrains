cd72b92162ab3e2486097338a84610a251e20245
Sergey Bukin <sergey.bukin@gmail.com>
2022-10-21 12:53:02 +0300

    Commit 12 Homework task10/13/15

---
+++ homework/task13/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+//  что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+System.Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine());
+
+if (number < 100)
+{
+    System.Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    int coef = 10;
+
+    if (number > 999)
+    {
+        while (true)
+        {
+            int sep = number / coef;
+
+             if (sep > 100 && sep < 1000)
+            {
+               break;
+            }
+            else
+            {
+              coef *= 10;
+            }
+        }
+        System.Console.WriteLine($"Третья цифра {(number / coef) % 10}");
+    }
+    
+    else
+    {
+      System.Console.WriteLine($"Третья цифра {number % 10}");
+    }
+    
+}
---
+++ homework/task15/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+System.Console.WriteLine("Введите день недели ");
+int day = int.Parse(Console.ReadLine());
+
+if(day <= 5)
+  {
+  System.Console.WriteLine("нет");
+  }
+  else
+  {
+  System.Console.WriteLine("да");
+  }
