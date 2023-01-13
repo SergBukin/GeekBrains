4d1cb110cf2450680b696d9a6bc2eb53aa111472
Sergey Bukin <sergey.bukin@gmail.com>
2023-01-13 18:42:48 +0300

    project diploma created

---
+++ diploma/final_project/Program.cs
@@ -0,0 +1,11 @@
+﻿string[] weekDays = new string[] { "Sun", "Mond", "Tue", "Wed", "Thu", "Frid", "Sa" };
+string result = "[";
+	for (int i = 0; i < weekDays.Length; i++)
+	{ 
+		if (weekDays[i].Length <= 3) result += $"{weekDays[i]}, ";
+		}
+
+	result.Trim(',');
+	result += "]";
+
+System.Console.WriteLine(result);
