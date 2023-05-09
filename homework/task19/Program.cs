780b0e0be13cc6f597964368a47a8bb5b26709d4
Sergey Bukin <sergey.bukin@gmail.com>
2023-05-09 12:42:52 +0300

    Commit 25 minor changes

--- homework/task19/Program.cs
+++ homework/task19/Program.cs
@@ -7,12 +7,12 @@
 System.Console.WriteLine("Введите 5-значное число");
 string number = Console.ReadLine();
 
-if(number[0] == number[4] && number[1] == number[3])
+if (number[0] == number[4] && number[1] == number[3])
 {
     System.Console.WriteLine("да");
 }
 else
 {
-System.Console.WriteLine("нет");
+    System.Console.WriteLine("нет");
 }
 
